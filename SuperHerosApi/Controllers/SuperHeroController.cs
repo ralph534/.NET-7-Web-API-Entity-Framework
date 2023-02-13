9058a27edb51878e9a236ff6c3eebe8875a364fd
Ralph Etienne <ralphyetienne@yahoo.com>
2023-02-13 12:09:19 -0500

    created http get request from ID to retrieve info on specific hero and to add a new hero to the list

--- SuperHerosApi/Controllers/SuperHeroController.cs
+++ SuperHerosApi/Controllers/SuperHeroController.cs
@@ -44,7 +44,34 @@ namespace SuperHerosApi.Controllers
         public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
         {
 			var hero = superHeroes.Find(x => x.Id == id);
+			if (hero is null)
+				return NotFound("Sorry, but hero doesnt exist.");		
             return Ok(hero);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<SuperHero>> AddHero(SuperHero hero)
+        {
+            superHeroes.Add(hero);
+            return Ok(superHeroes);
+        }
+
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(int id, SuperHero request)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null)
+                return NotFound("Sorry, but hero doesnt exist.");
+
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Name = request.Name;
+            hero.Place = request.Place;
+
+            return Ok(superHeroes);
         }
     }
 }
